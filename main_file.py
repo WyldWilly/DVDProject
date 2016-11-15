8226e55ede4cbca8f4a4f44717ad4ac60dc3cafa
Jebidiah T <wm.costantini@comcast.net>
2016-11-14 19:57:06 -0500

    Updates

--- main_file.py
+++ main_file.py
@@ -6,15 +6,23 @@ myPaths=["E:/DVDTemp/",
 "H:/DVDTemp/",
 "I:/DVD_Archives/",
 "J:/DVD_Archives/",
-"Z:/DVD_Hold/"]
+"Z:/DVD_Hold/",
+"Z:/DVD_Hold2",
+"Z:/DVD_Hold3"]
 
-import os, sys, json
+import os, sys, json, time
 
 fo = open("DVDs.txt", "w")
+fo.write("--=== DVD Files" + " ")
+fo.write(time.strftime("%d/%m/%Y"))
+fo.write(" " + time.strftime("%H:%M:%S") + " ===--")
+fo.write("\n" + "-----------------------------------------" + "\n")
+
 
 for i in myPaths:
     dirs = os.listdir( i )
     for file in dirs:
-        fo.write(i,file);
-        
+    	fo.write(i + " " + file + "\n");
+    fo.write("\n" + "==*==*==*==*==*==*==*==*==*==" + "\n")	
 fo.close()
+
